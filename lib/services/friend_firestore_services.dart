a0d4af129f54187c65d4bdc617c0514ef253f97b
Sulabh Shrestha <shresthasulabh777@gmail.com>
2024-04-08 16:07:19 +0545

    Friend added reflected on both user

--- lib/services/friend_firestore_services.dart
+++ lib/services/friend_firestore_services.dart
@@ -39,13 +39,19 @@ class FriendFirestoreServices {
   }
 
   void acceptFriendRequest({required String senderID}) {
-    // first removing
+    // first removingwe
     removeFriendRequest(senderID: senderID);
 
     // adding to friends collection
     friendsRef.doc(userID).set({
       "uid": userID,
       "friends": FieldValue.arrayUnion([senderID])
+    }, SetOptions(merge: true));
+
+    // adding to friends collection of sender
+    friendsRef.doc(senderID).set({
+      "uid": senderID,
+      "friends": FieldValue.arrayUnion([userID])
     }, SetOptions(merge: true));
   }
 

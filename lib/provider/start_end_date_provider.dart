e4d1affbdf0b165d3c7bdd5257ea65c0571b04f6
Sulabh Shrestha <shresthasulabh777@gmail.com>
2024-04-12 12:06:36 +0545

    FIxed issue with bar graph of sleep page

--- lib/provider/start_end_date_provider.dart
+++ lib/provider/start_end_date_provider.dart
@@ -10,7 +10,7 @@ class StartEndDateProvider with ChangeNotifier {
     this.startDate = startDate;
     this.endDate = endDate;
 
-    log("Start date: $startDate, End date: $endDate");
+    log("Set date called");
 
     notifyListeners();
   }
@@ -22,6 +22,10 @@ class StartEndDateProvider with ChangeNotifier {
         i.isBefore(endDate.add(const Duration(days: 1)));
         i = i.add(const Duration(days: 1))) {
       dateList.add(i.toString().substring(0, 10));
+
+      if (i.day == endDate.day) {
+        break;
+      }
     }
     return dateList;
   }
@@ -33,6 +37,10 @@ class StartEndDateProvider with ChangeNotifier {
         i.isBefore(endDate.add(Duration(days: 1)));
         i = i.add(Duration(days: 1))) {
       dayList.add(i.day.toString());
+
+      if (i.day == endDate.day) {
+        break;
+      }
     }
 
     return dayList;
